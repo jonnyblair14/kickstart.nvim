27abc68412855a6f6ec27370daf1fd5d4e4bdcd2
jonnyblair14 <jonny@alpha9o1.com>
2026-05-24 21:45:00 +0100

    attempted to add custom plugins

---
+++ lua/custom/plugins/rainbow-paren.lua
@@ -0,0 +1,16 @@
+return {
+	-- Auto Pairs
+	{
+		"windwp/nvim-autopairs"
+	},
+
+	-- Indentation Highlighting
+	{
+		"lukas-reineke/indent-blankline.nvim",
+	},
+
+	-- Rainbow Highlighting
+	{
+		"HiPhish/nvim-ts-rainbow2",
+	},
+}
