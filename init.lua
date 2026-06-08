d28b9774ef22eeb6f33aad45023c064e936c371e
jonnyblair14 <jonny@alpha9o1.com>
2026-06-08 21:01:52 +0000

    added html and js formatting

--- init.lua
+++ init.lua
@@ -621,7 +621,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        html = {},
+        ts_ls = {},
 
         -- stylua = {}, -- Used to format Lua code
 
